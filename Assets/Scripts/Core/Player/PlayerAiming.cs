393a522853acd74c6392d5eaeb631fd9c044fd71
Warakom <warakom247365@gmail.com>
2024-06-22 17:29:39 +0200

    Aiming

---
+++ Assets/Scripts/Core/Player/PlayerAiming.cs
@@ -0,0 +1,23 @@
+﻿using Input;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Core.Player
+{
+    public class PlayerAiming : NetworkBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private InputReader inputReader;
+        [SerializeField] private Transform turretTransform;
+
+        private void LateUpdate()
+        {
+            if (!IsOwner) return;
+
+            var aimWorldPosition = Camera.main.ScreenToWorldPoint(inputReader.AimPosition);
+            turretTransform.up = new Vector2(
+                aimWorldPosition.x - turretTransform.position.x,
+                aimWorldPosition.y - turretTransform.position.y);
+        }
+    }
+}
--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -10,6 +10,8 @@ namespace Input
         public event Action<Vector2> MoveEvent = delegate { };
         public event Action<bool> PrimaryFireEvent = delegate { };
         
+        public Vector2 AimPosition { get; private set; }
+        
         private Controls _controls;
 
         private void OnEnable()
@@ -24,5 +26,6 @@ namespace Input
 
         public void OnMove(InputAction.CallbackContext context) => MoveEvent.Invoke(context.ReadValue<Vector2>());
         public void OnPrimaryFire(InputAction.CallbackContext context) => PrimaryFireEvent.Invoke(context.performed);
+        public void OnAim(InputAction.CallbackContext context) => AimPosition = context.ReadValue<Vector2>();
     }
 }
