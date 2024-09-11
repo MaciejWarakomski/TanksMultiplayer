b7afd07ad0ad58a29672bdf27da5be58445cf5e7
Warakom <warakom247365@gmail.com>
2024-09-11 22:45:56 +0200

    Player name selection

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class NameSelector : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField nameField;
+        [SerializeField] private Button connectButton;
+        [SerializeField] private int minNameLength = 1;
+        [SerializeField] private int maxNameLength = 12;
+        
+        private const string PlayerNameKey = "PlayerName";
+        
+        private void Start()
+        {
+            if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                return;
+            }
+            
+            nameField.text = PlayerPrefs.GetString(PlayerNameKey, nameField.text);
+            HandleNameChange();
+        }
+
+        public void HandleNameChange()
+        {
+            connectButton.interactable = 
+                nameField.text.Length >= minNameLength && 
+                nameField.text.Length <= maxNameLength;
+        }
+
+        public void Connect()
+        {
+            PlayerPrefs.SetString(PlayerNameKey, nameField.text);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
